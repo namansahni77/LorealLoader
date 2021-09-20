45f944c0e10a226a255db4192688dd42fc6912e0
namansahni77 <naman.sahni@gamingcentral.in>
2021-09-20 04:18:26 -0700

    loading commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+import './App.css';
+import Loader from './components/Loader';
+function App() {
+  return (
+    <Loader/>
+  );
+}
+
+export default App;
---
+++ src/components/Loader.js
@@ -0,0 +1,28 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import './Loader.css';
+const Loader =()=>{
+    const[count,setCount]=useState(0);
+useEffect(()=>{
+    const handler = setInterval(()=>{
+          setCount(oldvalue=>{
+              const val =oldvalue+1
+              if(val>=100){
+                clearInterval(handler);
+                console.log(handler)
+              }
+              return val
+            })
+        console.log(count);
+      },95)
+},[])
+  
+return(
+    <Fragment>
+        <div className="center">
+<h1 >Loreal</h1>
+<div className='num' >{count + '%'}</div>
+</div>
+    </Fragment>
+);
+}
+export default Loader;
